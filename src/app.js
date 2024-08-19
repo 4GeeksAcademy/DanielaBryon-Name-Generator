1f88326f9eee73c75a8d54f5f12262880cb056fa
Daniela Bryon <daniellabryren@icloud.com>
2024-08-19 11:21:22 +0000

    first commit domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our", "del", "tu"];
+  let adjectives = ["great", "big", "pued", "ugly"];
+  let nouns = ["jogger", "racoon", "icio", "inter"];
+  let extensions = [".com", ".es", ".net", ".us"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let ext of extensions) {
+          domainNames.push(`${pronoun}${adjective}${noun}${ext}`);
+        }
+      }
+    }
+    const random = array => array[Math.floor(Math.random() * array.length)];
+    document.getElementById("click").onclick = function() {
+      document.getElementById("domain").innerHTML = random(domainNames);
+    };
+  }
 };
